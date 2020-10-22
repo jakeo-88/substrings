70ad63df185945c3e8148d623b0d090e8f1f6b21
Jake <jakegreen2016@gmail.com>
2020-10-22 07:47:52 -0800

    create Ruby file with pseudo code

---
+++ substrings.rb
@@ -0,0 +1,9 @@
+#Create a variable for the value to search
+
+#Create a variable to hold the dictionary array values 
+
+#Check if value matches a word or sub words are in the  dictionary
+
+#Tally the number of matches found from the dictionary
+
+#Print the total tally amount to the console
