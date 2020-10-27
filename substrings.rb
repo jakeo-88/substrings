d7d40edbf04e733e9d83b6fee5e55547616ffe83
Jake <jakegreen2016@gmail.com>
2020-10-26 22:55:07 -0800

    make new array with strings and substrings

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,28 @@
-#Create a variable for the value to search
-variable = "below"
+#Format first argument so all letters are lowercase
+variable.downcase!
 
 #Create a variable to hold the dictionary array values 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 #Check if value matches a word or sub words are in the  dictionary
-dictionary.select!{|value| variable.include?(value)}
+    
+    #split up the words into an array
+    variable = variable.split(" ")
+
+    #for each element in array, find strings and substrings    
+    new_array = []
+
+    i = 0
+    while i < variable.length do
+    j = 0
+    while j < dictionary.length do
+        if variable[i].include?(dictionary[j])
+        new_array.push(dictionary[j])
+        end
+        j += 1
+    end
+    i += 1
+    end
 
 #Tally the number of matches found from the dictionary
 dictionary.tally
