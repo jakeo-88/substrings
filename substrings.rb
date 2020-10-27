f3833b4786edb81bd30003dc0e5f037bea603b35
Jake <jakegreen2016@gmail.com>
2020-10-26 23:02:08 -0800

    made code into a method

--- substrings.rb
+++ substrings.rb
@@ -1,31 +1,36 @@
-#Format first argument so all letters are lowercase
-variable.downcase!
+#Create method
+def substrings variable, dictionary
 
-#Create a variable to hold the dictionary array values 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    #Format first argument so all letters are lowercase
+    variable.downcase!
 
-#Check if value matches a word or sub words are in the  dictionary
-    
-    #split up the words into an array
-    variable = variable.split(" ")
+    #Create a variable to hold the dictionary array values 
+    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-    #for each element in array, find strings and substrings    
-    new_array = []
+    #Check if value matches a word or sub words are in the  dictionary
+        
+        #split up the words into an array
+        variable = variable.split(" ")
 
-    i = 0
-    while i < variable.length do
-    j = 0
-    while j < dictionary.length do
-        if variable[i].include?(dictionary[j])
-        new_array.push(dictionary[j])
+        #for each element in array, find strings and substrings    
+        new_array = []
+
+        i = 0
+        while i < variable.length do
+        j = 0
+        while j < dictionary.length do
+            if variable[i].include?(dictionary[j])
+            new_array.push(dictionary[j])
+            end
+            j += 1
         end
-        j += 1
-    end
-    i += 1
-    end
+        i += 1
+        end
 
-#Tally the number of matches found from the dictionary
-word_count = new_array.reduce((Hash.new(0))) do |accumulator, iteration|
-    accumulator[iteration] += 1
-    accumulator
-  end
+    #Tally the number of matches found from the dictionary
+    word_count = new_array.reduce((Hash.new(0))) do |accumulator, iteration|
+        accumulator[iteration] += 1
+        accumulator
+    end
+    print word_count
+end
